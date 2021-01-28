4f2bf97ce5cb6a8e788afe20590a2433bf37401c
Carla <carla.holcomb25@gmail.com>
2021-01-27 21:58:40 -0500

    updated controllers

--- backend/app/channels/messages_channel.rb
+++ backend/app/channels/messages_channel.rb
@@ -1,11 +1,13 @@
 class MessagesChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
-    conversation = Conversation.find(params[:conversation])
-    stream_for conversation
-  end
+      stream_from "room_channel"
+    end
 
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
+    def unsubscribed
+      # Any cleanup needed when channel is unsubscribed
+    end
+
+    def speak(data)
+      Message.create! content: data['message']
+    end
 end
--- backend/app/controllers/messages_controller.rb
+++ backend/app/controllers/messages_controller.rb
@@ -1,12 +1,15 @@
 class MessagesController < ApplicationController
+  def index
+    messages = Message.all
+    render json: messages
+  end
+
   def create
     message = Message.new(message_params)
-    conversation = Conversation.find(message_params[:conversation_id])
     if message.save
-      serialized_data = ActiveModelSerializers::Adapter::Json.new(
-        MessageSerializer.new(message)
-      ).serializable_hash
-      MessagesChannel.broadcast_to conversation, serialized_data
+      @message = Message.create(message_params)
+      render json: @message
+      ActionCable.server.broadcast 'room_channel', serialized_data
       head :ok
     end
   end
@@ -14,6 +17,6 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:text, :conversation_id)
+    params.require(:message)
   end
 end
